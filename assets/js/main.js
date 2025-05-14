ad64720224ef8dc4eff52452e8f01dac395edef3
abutahirhasan <athasan242@gmail.com>
2025-05-15 00:15:14 +0600

    rudra-theme

---
+++ assets/js/main.js
@@ -0,0 +1,112 @@
+
+
+(function($) {
+    "use strict";
+    
+
+    $(document).ready( function() {
+
+        //>> Mobile Menu Js Start <<//
+        $('#mobile-menu').meanmenu({
+            meanMenuContainer: '.mobile-menu',
+            meanScreenWidth: "1199",
+            meanExpand: ['<i class="far fa-plus"></i>'],
+        });
+
+        //>> Sidebar Toggle Js Start <<//
+        $(".offcanvas__close,.offcanvas__overlay").on("click", function() {
+            $(".offcanvas__info").removeClass("info-open");
+            $(".offcanvas__overlay").removeClass("overlay-open");
+        });
+        $(".sidebar__toggle").on("click", function() {
+            $(".offcanvas__info").addClass("info-open");
+            $(".offcanvas__overlay").addClass("overlay-open");
+        });
+
+        //>> Body Overlay Js Start <<//
+        $(".body-overlay").on("click", function() {
+            $(".offcanvas__area").removeClass("offcanvas-opened");
+            $(".df-search-area").removeClass("opened");;
+            $(".body-overlay").removeClass("opened");
+        });
+
+        //>> Sticky Header Js Start <<//
+
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 250) {
+                $("#header-sticky").addClass("sticky");
+            } else {
+                $("#header-sticky").removeClass("sticky");
+            }
+        });
+
+        //>> Video Popup Start <<//
+        $(".img-popup").magnificPopup({
+            type: "image",
+            gallery: {
+                enabled: true,
+            },
+        });
+
+        $('.video-popup').magnificPopup({
+            type: 'iframe',
+            callbacks: {
+            }
+        });
+        
+        //>> Counterup Start <<//
+        $(".count").counterUp({
+            delay: 15,
+            time: 4000,
+        });
+
+        //>> Brand Slider Start <<//
+        if($('.nano-testimonial-slider').length > 0) {
+            const NanoTestimonialSlider = new Swiper(".nano-testimonial-slider", {
+                spaceBetween: 30,
+                speed: 2000,
+                loop: true,
+                autoplay: {
+                    delay: 1000,
+                    disableOnInteraction: false,
+                },
+                breakpoints: {
+                    1399: {
+                        slidesPerView: 1,
+                    },
+                    1199: {
+                        slidesPerView: 1,
+                    },
+                    991: {
+                        slidesPerView: 1,
+                    },
+                    767: {
+                        slidesPerView: 1,
+                    },
+                    575: {
+                        slidesPerView: 1,
+                    },
+                    400: {
+                        slidesPerView: 1,
+                    },
+                },
+            });
+        }
+
+        //>> Wow Animation Start <<//
+        new WOW().init();
+
+        //>> Nice Select Start <<//
+        if ($('.single-select').length) {
+            $('.single-select').niceSelect();
+        }
+
+        
+
+    }); // End Document Ready Function
+
+   
+   
+
+})(jQuery); // End jQuery
+
