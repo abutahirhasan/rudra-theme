0e89c325788f7579fb7c42b304667dfc93836af1
abutahirhasan <athasan242@gmail.com>
2025-05-19 01:27:09 +0600

    header1

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,105 +5,133 @@
     
 
     $(document).ready( function() {
+			//>> Mobile Menu Js Start <<//
+			$("#mobile-menu").meanmenu({
+				meanMenuContainer: ".mobile-menu",
+				meanScreenWidth: "1199",
+				meanExpand: ['<i class="far fa-plus"></i>'],
+			});
 
-        //>> Mobile Menu Js Start <<//
-        $('#mobile-menu').meanmenu({
-            meanMenuContainer: '.mobile-menu',
-            meanScreenWidth: "1199",
-            meanExpand: ['<i class="far fa-plus"></i>'],
-        });
+			//>> Sidebar Toggle Js Start <<//
+			$(".offcanvas__close,.offcanvas__overlay").on("click", function () {
+				$(".offcanvas__info").removeClass("info-open");
+				$(".offcanvas__overlay").removeClass("overlay-open");
+			});
+			$(".sidebar__toggle").on("click", function () {
+				$(".offcanvas__info").addClass("info-open");
+				$(".offcanvas__overlay").addClass("overlay-open");
+			});
 
-        //>> Sidebar Toggle Js Start <<//
-        $(".offcanvas__close,.offcanvas__overlay").on("click", function() {
-            $(".offcanvas__info").removeClass("info-open");
-            $(".offcanvas__overlay").removeClass("overlay-open");
-        });
-        $(".sidebar__toggle").on("click", function() {
-            $(".offcanvas__info").addClass("info-open");
-            $(".offcanvas__overlay").addClass("overlay-open");
-        });
+			//>> Body Overlay Js Start <<//
+			$(".body-overlay").on("click", function () {
+				$(".offcanvas__area").removeClass("offcanvas-opened");
+				$(".df-search-area").removeClass("opened");
+				$(".body-overlay").removeClass("opened");
+			});
 
-        //>> Body Overlay Js Start <<//
-        $(".body-overlay").on("click", function() {
-            $(".offcanvas__area").removeClass("offcanvas-opened");
-            $(".df-search-area").removeClass("opened");;
-            $(".body-overlay").removeClass("opened");
-        });
+			//>> Sticky Header Js Start <<//
 
-        //>> Sticky Header Js Start <<//
+			$(window).scroll(function () {
+				if ($(this).scrollTop() > 250) {
+					$("#header-sticky").addClass("sticky");
+				} else {
+					$("#header-sticky").removeClass("sticky");
+				}
+			});
 
-        $(window).scroll(function() {
-            if ($(this).scrollTop() > 250) {
-                $("#header-sticky").addClass("sticky");
-            } else {
-                $("#header-sticky").removeClass("sticky");
-            }
-        });
+			//>> Video Popup Start <<//
+			$(".img-popup").magnificPopup({
+				type: "image",
+				gallery: {
+					enabled: true,
+				},
+			});
 
-        //>> Video Popup Start <<//
-        $(".img-popup").magnificPopup({
-            type: "image",
-            gallery: {
-                enabled: true,
-            },
-        });
+			$(".video-popup").magnificPopup({
+				type: "iframe",
+				callbacks: {},
+			});
 
-        $('.video-popup').magnificPopup({
-            type: 'iframe',
-            callbacks: {
-            }
-        });
-        
-        //>> Counterup Start <<//
-        $(".count").counterUp({
-            delay: 15,
-            time: 4000,
-        });
+			//>> Counterup Start <<//
+			$(".count").counterUp({
+				delay: 15,
+				time: 4000,
+			});
 
-        //>> Brand Slider Start <<//
-        if($('.nano-testimonial-slider').length > 0) {
-            const NanoTestimonialSlider = new Swiper(".nano-testimonial-slider", {
-                spaceBetween: 30,
-                speed: 2000,
-                loop: true,
-                autoplay: {
-                    delay: 1000,
-                    disableOnInteraction: false,
-                },
-                breakpoints: {
-                    1399: {
-                        slidesPerView: 1,
-                    },
-                    1199: {
-                        slidesPerView: 1,
-                    },
-                    991: {
-                        slidesPerView: 1,
-                    },
-                    767: {
-                        slidesPerView: 1,
-                    },
-                    575: {
-                        slidesPerView: 1,
-                    },
-                    400: {
-                        slidesPerView: 1,
-                    },
-                },
-            });
-        }
+			//>> Brand Slider Start <<//
+			if ($(".nano-testimonial-slider").length > 0) {
+				const NanoTestimonialSlider = new Swiper(
+					".nano-testimonial-slider",
+					{
+						spaceBetween: 30,
+						speed: 2000,
+						loop: true,
+						autoplay: {
+							delay: 1000,
+							disableOnInteraction: false,
+						},
+						breakpoints: {
+							1399: {
+								slidesPerView: 1,
+							},
+							1199: {
+								slidesPerView: 1,
+							},
+							991: {
+								slidesPerView: 1,
+							},
+							767: {
+								slidesPerView: 1,
+							},
+							575: {
+								slidesPerView: 1,
+							},
+							400: {
+								slidesPerView: 1,
+							},
+						},
+					}
+				);
+			}
 
-        //>> Wow Animation Start <<//
-        new WOW().init();
+			//>> Wow Animation Start <<//
+			new WOW().init();
 
-        //>> Nice Select Start <<//
-        if ($('.single-select').length) {
-            $('.single-select').niceSelect();
-        }
+			//>> Nice Select Start <<//
+			if ($(".single-select").length) {
+				$(".single-select").niceSelect();
+			}
 
-        
+			//>> Search Popup Start <<//
+			const $searchWrap = $(".search-wrap");
+			const $navSearch = $(".nav-search");
+			const $searchClose = $("#search-close");
 
-    }); // End Document Ready Function
+			$(".search-trigger").on("click", function (e) {
+				e.preventDefault();
+				$searchWrap.animate({ opacity: "toggle" }, 500);
+				$navSearch.add($searchClose).addClass("open");
+			});
+
+			$(".search-close").on("click", function (e) {
+				e.preventDefault();
+				$searchWrap.animate({ opacity: "toggle" }, 500);
+				$navSearch.add($searchClose).removeClass("open");
+			});
+
+			function closeSearch() {
+				$searchWrap.fadeOut(200);
+				$navSearch.add($searchClose).removeClass("open");
+			}
+
+			$(document.body).on("click", function (e) {
+				closeSearch();
+			});
+
+			$(".search-trigger, .main-search-input").on("click", function (e) {
+				e.stopPropagation();
+			});
+		}); // End Document Ready Function
 
    
    
